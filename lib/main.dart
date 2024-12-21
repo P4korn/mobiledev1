37fc9610594b11fdfcbba06fa32cf918f5effed8
Pakorn Surakarnkul <pakorn.surakarn@gmail.com>
2024-12-21 16:00:39 +0700

    main page update

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              'Hello mtfk',
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.purple,
+          ),
+          body: Container(
+              height: 100,
+              width: 100,
+              color: Colors.orange,
+              padding: const EdgeInsets.only(top: 40),
+              margin: const EdgeInsets.all(20),
+              child: const Text(
+                'Hello',
+                style: TextStyle(color: Colors.black),
+                textAlign: TextAlign.center,
+              )),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print('Hello');
+            },
+            child: const Icon(Icons.phone),
+          ),
+        ));
   }
 }
